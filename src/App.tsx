3a3ce7b13d9d9edf66cadddd6b1b8c07a68dd8d2
Pau Torrents <pau.torrents@thoughtworks.com>
2020-04-07 23:54:04 +0200

    Add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,30 @@
 import React from 'react'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom'
+
+import { LandingPage } from './pages/LandingPage/LandingPage'
+import { LoginPage } from './pages/LoginPage/LoginPage'
 
 import './App.scss'
 
 const App = () => {
   return (
-    <div>
-      Hackovid Professors!
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/login">
+          <LoginPage />
+        </Route>
+        <Route exact path="/">
+          <LandingPage />
+        </Route>
+        <Route>
+          <div>Not found :(</div>
+        </Route>
+      </Switch>
+    </Router>
   )
 }
 
---
+++ src/pages/LandingPage/LandingPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const LandingPage = () => {
+  return (
+    <div>
+      Welcome to Hackovid Professors! :)
+    </div>
+  )
+}
+
+export { LandingPage }
