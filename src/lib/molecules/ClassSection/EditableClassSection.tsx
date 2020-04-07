1407d89428b3bb6b70a63d40417e779045510179
Pau Torrents <pau.torrents@thoughtworks.com>
2020-04-08 01:48:50 +0200

    Add CKEditor

---
+++ src/lib/molecules/ClassSection/EditableClassSection.tsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react'
+// @ts-ignore
+import CKEditor from '@ckeditor/ckeditor5-react'
+// @ts-ignore
+import InlineEditor from '@ckeditor/ckeditor5-build-inline'
+
+import './EditableClassSection.scss'
+
+interface EditableClassSection {
+  onBlur: (htmlContent: string) => void
+}
+
+const EditableClassSection = ({
+  onBlur
+}: EditableClassSection) => {
+  const [data, setData] = useState<string>('')
+
+  return (
+    <div className="editable-class-section">
+      <h3>Section</h3>
+      <div className="editable-class-section__editor">
+        <CKEditor
+          editor={InlineEditor}
+          data={data}
+          onChange={(event: any, editor: { getData: () => string }) => {
+            setData(editor.getData())
+          }}
+          onBlur={() => onBlur(data)}
+        />
+      </div>
+    </div>
+  )
+}
+
+export { EditableClassSection }
