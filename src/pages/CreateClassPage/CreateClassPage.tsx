64461296fda2afedac4a70e070029b8062ac0f3f
Pau Torrents <pau.torrents@thoughtworks.com>
2020-04-08 13:54:14 +0200

    Add saved sections

--- src/pages/CreateClassPage/CreateClassPage.tsx
+++ src/pages/CreateClassPage/CreateClassPage.tsx
@@ -6,6 +6,8 @@ import { Container } from '../../lib/atoms/Container/Container'
 import { ClassSection } from '../../model/ClassSection'
 
 import './CreateClassPage.scss'
+import { Row } from '../../lib/atoms/Row/Row'
+import { Col } from '../../lib/atoms/Col/Col'
 
 const CreateClassPage = () => {
   const [ nextSectionId, setNextSectionId ] = useState<number>(0)
@@ -34,42 +36,65 @@ const CreateClassPage = () => {
     setSections(sections.filter((currentSection) => currentSection.id !== section.id))
   }
 
-  return (
-    <Container className="create-class">
-      <div className="create-class__title">
-        <EditableText
-          placeholder="Títol de la classe..."
-          value={title}
-          onChange={setTitle}
-          className="create-class__title__editable"
-        />
-      </div>
-
-      {sections.map((section) => (
-        <div className="create-class__section" key={`editable-section-${section.id}`}>
-          <EditableClassSection
-            renderRight={(
-              <Button
-                icon="trash"
-                intent={Intent.DANGER}
-                onClick={() => deleteSection(section)}
-              >
-                Esborrar secció
-              </Button>
-            )}
-            section={section}
-            onChange={updateSection}
+  const renderEditableArea = () => {
+    return (
+      <div className="create-class__editable">
+        <div className="create-class__title">
+          <EditableText
+            placeholder="Títol de la classe..."
+            value={title}
+            onChange={setTitle}
+            className="create-class__title__editable"
           />
         </div>
-      ))}
 
-      <div className="create-class__actions">
-        <Button
-          onClick={addSection}
-          icon="plus"
-          intent={Intent.PRIMARY}
-        >Afegir secció</Button>
+        {sections.map((section) => (
+          <div className="create-class__section" key={`editable-section-${section.id}`}>
+            <EditableClassSection
+              renderRight={(
+                <Button
+                  icon="trash"
+                  intent={Intent.DANGER}
+                  onClick={() => deleteSection(section)}
+                >
+                  Esborrar secció
+                </Button>
+              )}
+              section={section}
+              onChange={updateSection}
+            />
+          </div>
+        ))}
+
+        <div className="create-class__actions">
+          <Button
+            onClick={addSection}
+            icon="plus"
+            intent={Intent.PRIMARY}
+          >Afegir secció</Button>
+        </div>
       </div>
+    )
+  }
+
+  const renderFavoritesArea = () => {
+    return (
+      <div className="create-class__favorites">
+        <h1>Seccions guardades</h1>
+      </div>
+    )
+  }
+
+  return (
+    <Container className="create-class">
+      <Row>
+        <Col md={8} className="create-class__editable-column">
+          {renderEditableArea()}
+        </Col>
+        <Col md={4} className="create-class__favorites-column">
+          {renderFavoritesArea()}
+        </Col>
+      </Row>
     </Container>
   )
 }
