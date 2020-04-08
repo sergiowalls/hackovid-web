344292d6b9fd82450e4abc1068c88f15407a2057
Pau Torrents <pau.torrents@thoughtworks.com>
2020-04-08 02:01:06 +0200

    Modify new page

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,7 @@ const App = () => {
           <Route path="/login">
             <LoginPage />
           </Route>
-          <Route path="/classes/create">
+          <Route path="/classes/new">
             <CreateClassPage />
           </Route>
           <Route exact path="/">
