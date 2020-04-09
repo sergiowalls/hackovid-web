72d36c440e6d5637afe03519f438985d99237302
Pau Torrents <pau.torrents@thoughtworks.com>
2020-04-09 15:42:41 +0200

    Login

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useStoreon } from 'storeon/react'
-import { Button } from '@blueprintjs/core'
-import { useHistory } from 'react-router-dom'
+import { Button, Card, Divider, Elevation, FormGroup, InputGroup } from '@blueprintjs/core'
+import { Link, useHistory } from 'react-router-dom'
 
 import { PublicPage } from '../../lib/molecules/PublicPage/PublicPage'
 import { Container } from '../../lib/atoms/Container/Container'
@@ -9,26 +9,75 @@ import { SafePageView } from '../../lib/molecules/SafePageView/SafePageView'
 import { State } from '../../store/state/State'
 import { Events } from '../../store/event/Events'
 
+import './LoginPage.scss'
+
+interface CardSectionProps {
+  children?: any
+  className?: string
+}
+
+const CardSection = ({
+  children,
+  className
+}: CardSectionProps) => {
+  return (
+    <div className={`card-section ${className}`}>
+      {children}
+    </div>
+  )
+}
+
 const LoginPage = () => {
-  const history = useHistory()
-  const { dispatch, auth: { error } } = useStoreon<State, Events>('auth')
+  const { dispatch } = useStoreon<State, Events>('auth')
 
   const handleOnLoginClick = () => {
     dispatch('auth/login', { username: 'test', password: 'test' })
   }
 
   return (
-    <PublicPage>
+    <PublicPage className="login-page">
       <Container>
         <SafePageView>
-          <div>
-            Login Page
-          </div>
-          <Button onClick={handleOnLoginClick}>Entrar</Button>
-          <Button onClick={() => history.push('/register')}>Registra't</Button>
-          <div>
-            {error}
-          </div>
+          <Card className="login-page__card" elevation={Elevation.FOUR}>
+
+            <CardSection className="login-page__card__header">
+              Entra
+            </CardSection>
+
+            <Divider className="login-page__card-divider" />
+
+            <CardSection>
+              <FormGroup
+                label="Adreça de correu electrònic"
+                labelFor="email-input"
+              >
+                <InputGroup
+                  id="email-input"
+                  placeholder="exemple@exemple.com"
+                  type="email"
+                />
+              </FormGroup>
+              <FormGroup
+                label="Contrassenya"
+                labelFor="password-input"
+              >
+                <InputGroup
+                  id="password-input"
+                  placeholder="****"
+                  type="password"
+                />
+              </FormGroup>
+            </CardSection>
+
+            <CardSection className="login-page__card__actions">
+              <Button
+                large={true}
+                onClick={handleOnLoginClick}
+                icon="log-in"
+              >Entrar</Button>
+              <div className="login-page__card__actions__alternative">Encara no tens compte? <Link to="/register">Registra't</Link></div>
+            </CardSection>
+          </Card>
         </SafePageView>
       </Container>
     </PublicPage>
