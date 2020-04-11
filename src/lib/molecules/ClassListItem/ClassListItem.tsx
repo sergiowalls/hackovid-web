e5720832d73a470623aede165f046829194ac992
Pau Torrents <pau.torrents@thoughtworks.com>
2020-04-11 12:07:50 +0200

    Some styling

--- src/lib/molecules/ClassListItem/ClassListItem.tsx
+++ src/lib/molecules/ClassListItem/ClassListItem.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
+import { Card, Elevation } from '@blueprintjs/core'
 
 import { Class } from '../../../model/Class'
 
 import './ClassListItem.scss'
-import { Card } from '@blueprintjs/core'
 
 interface ClassListItemProps {
   classEntity: Class
@@ -12,8 +12,8 @@ interface ClassListItemProps {
 const ClassListItem = ({ classEntity }: ClassListItemProps) => {
 
   return (
-    <Card className="class-list-item">
-      <h4>{classEntity.header.title}</h4>
+    <Card className="class-list-item" elevation={Elevation.TWO}>
+      <div className="class-list-item__title">{classEntity.header.title}</div>
 
       <div className="class-list-item__sections">
         {classEntity.sections.map(section => (
