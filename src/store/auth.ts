da4c780c4925a25539de7ca9d6ed45613762065c
Pau Torrents <pau.torrents@thoughtworks.com>
2020-04-12 22:33:07 +0200

    Save user

--- src/store/auth.ts
+++ src/store/auth.ts
@@ -2,13 +2,18 @@ import { StoreonModule, StoreonStore } from 'storeon'
 import { State } from './state/State'
 import { AuthenticateEvent, Events } from './event/Events'
 import { AuthToken } from '../model/AuthToken'
+import { User } from '../model/User'
 
 export const auth: StoreonModule<State, Events> = (store: StoreonStore<State, Events>) => {
   store.on('@init', (state: State): State => {
-    const authTokenStringValue = localStorage.getItem('authToken')
 
+    const authTokenStringValue = localStorage.getItem('authToken')
     const authTokenValue = authTokenStringValue ? JSON.parse(authTokenStringValue) : undefined
 
+    const userStringValue = localStorage.getItem('user')
+    const userValue = userStringValue ? JSON.parse(userStringValue) : undefined
+
+    const user = userValue ? new User(userValue.id, userValue.username, userValue.institution, userValue.name) : undefined
     const authToken = authTokenValue ? new AuthToken(authTokenValue.token) : undefined
     const isAuthenticated = authTokenValue !== undefined
 
@@ -16,7 +21,8 @@ export const auth: StoreonModule<State, Events> = (store: StoreonStore<State, Ev
       ...state,
       auth: {
         isAuthenticated,
-        authToken
+        authToken,
+        user
       }
     }
   })
@@ -24,7 +30,8 @@ export const auth: StoreonModule<State, Events> = (store: StoreonStore<State, Ev
   store.on('auth/authenticate', (state: State, event: AuthenticateEvent): State => {
     console.log(event)
 
-    localStorage.setItem('authToken', JSON.stringify(event))
+    localStorage.setItem('authToken', JSON.stringify(event.token))
+    localStorage.setItem('user', JSON.stringify(event.user))
 
     return {
       ...state,
@@ -38,6 +45,7 @@ export const auth: StoreonModule<State, Events> = (store: StoreonStore<State, Ev
 
   store.on('auth/logout', (state: State): State => {
     localStorage.removeItem('authToken')
+    localStorage.removeItem('user')
 
     return {
       ...state,
